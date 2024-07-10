b94eddd793c4fb067f248d3ee8c9ea80b02ef313
Jeremy Gao <145064583+WenSimEHRP@users.noreply.github.com>
2024-07-09 17:34:34 -0700

    Update: install_dependencies.sh
    prs are merged into trunk

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -13,11 +13,4 @@ pip3 install -r requirements.txt
 
 git clone https://github.com/openttd/nml.git
 cd nml
-git config user.name "anonymous"
-git config user.email "anonymous@example.com"
-git checkout 522d1bc6a4f631e1d2f18d05ed5abb2fc8f8f494
-git remote add pr_source https://github.com/glx22/nml
-git fetch pr_source layout_registers
-git merge -X ours pr_source/layout_registers -m "Merge layout_registers"
-git fetch pr_source var6B
-git merge -X ours pr_source/var6B -m "Merge var6B"
+git checkout dcbfdc7a771e6b3033a9e09c656ac3c441f466db
