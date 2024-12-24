f333779744cea5698a728cd12c6167cd8e4a80a6
Jeremy Gao <145064583+WenSimEHRP@users.noreply.github.com>
2024-12-24 01:06:29 -0800

    Update: dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,14 +3,7 @@
 # here I'm using ahyangyi's fork of gorender
 
 export GOPATH=$PWD/gopath
-go install github.com/ahyangyi/gorender/cmd@daeb83b4
+go install github.com/ahyangyi/gorender/cmd@cdca513
 mv gopath/bin/cmd gopath/bin/gorender
 
-# install python dependencies
-pip3 install -r requirements.txt
-# nml requires python to run
-# here I merge two PRs to nml, #306 and #320
-
 git clone https://github.com/openttd/nml.git
-cd nml
-git checkout f00eefa0434751ae669fb153f147e79420e35a5a
